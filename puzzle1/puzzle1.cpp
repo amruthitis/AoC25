62bbb78ce2883fba37cf00c69caf9b3a5f06cbe1
amruthitis <amruthteki@gmail.com>
2025-12-06 23:09:18 +0530

    puzzle 1 part 2 solved

--- puzzle1/puzzle1.cpp
+++ puzzle1/puzzle1.cpp
@@ -45,8 +45,71 @@ class Password{
 
 };
 
+// Part2 code
+
+class Password2{
+    public:
+    int arr[SIZE];
+    int counter = 0;
+    
+    public:
+
+    Password2(std::ifstream& input){
+        for(int i=0;i<100;i++){
+            arr[i] = i;
+        }
+        int pointer = arr[50];
+
+        std::string code;
+        char indicator;
+        int value;
+
+        if(input.is_open()){
+            while(getline(input, code)){
+                indicator = code[0];
+                value = std::stoi(code.substr(1));
+                int counter2 = 0;
+                if(indicator == 'L'){  
+                    int newpointer = (pointer - value)%100;
+                    while(counter2 < value){  
+                        if(pointer == 0){
+                            counter++;
+                        }
+                        pointer--;
+                        if(pointer < 0){
+                            pointer = 99;
+                        }
+                        counter2++;
+                    }
+
+                }else if(indicator == 'R'){
+                    int newpointer2 = (pointer + value)%100;
+                    while (counter2 < value){
+                        if(pointer == 0){
+                            counter++;
+                        }
+                        pointer++;
+                        if(pointer > 99){
+                            pointer = 0;
+                        }
+                        counter2++;
+                    }
+                }
+            }
+            input.close();
+        }
+        else{
+            std::cerr << "error" << std::endl;
+        }
+        std::cout << counter << std::endl;
+    }
+
+};
+
+
+
 int main(){
-    std::ifstream input("p1test.txt");
-    Password password(input);
+    std::ifstream input("p1input.txt");
+    Password2 password2(input);
 }
 
